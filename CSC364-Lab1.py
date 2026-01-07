a0a9d464372bf909a63f17f9225ba42d3796ccd3
Kevin Lu <kevinlu3234@gmail.com>
2026-01-06 17:25:55 -0800

    setup

---
+++ CSC364-Lab1.py
@@ -0,0 +1,23 @@
+# [programname] [hostname] [port] [pings] [ping_bytes] [bulk_bytes]
+
+import sys
+
+
+
+
+WSAStartup()
+
+closesocket()
+
+#at end call
+WSACleanup()
+
+#for errors handling
+WSAGetLastError()
+
+getaddrinfo()
+
+send_all()
+
+recv_all()
+
