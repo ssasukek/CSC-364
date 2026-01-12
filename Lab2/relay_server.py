cc84fedf65a3d3c46a5c1ebdc6edc8e216eb0547
Kevin Lu <kevinlu3234@gmail.com>
2026-01-12 11:31:25 -0800

    lab2

---
+++ Lab2/relay_server.py
@@ -0,0 +1,47 @@
+### TCP Relay Server - that allow two clients to communicate through it ###
+
+import socket
+import threading
+
+PORT = 5000
+IP = '0.0.0.0'
+
+def send_all(sock, data: bytes):
+    total_sent = 0
+    length = len(data)
+
+    while total_sent < length:
+        sent = sock.send(data[total_sent:])
+        if sent <= 0:
+            raise RuntimeError("Socket connection broken")
+        total_sent += sent
+
+
+# def recv_all(sock: socket.socket, length: int):
+
+
+def main():
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind((IP, PORT))
+    serversocket.listen(5)
+    
+    print(f"Relay server started on port: {PORT}")
+
+    clientA, addrA = serversocket.accept()
+    print(f"Client A connected from {addrA}")
+
+    clientB, addrB = serversocket.accept()
+    print(f"Client B connected from {addrB}")
+
+    try:
+        send_all(clientA, b"READY\n")
+        send_all(clientB, b"READY\n")
+    except Exception as e:
+        clientA.close()
+        clientB.close()
+        return
+
+
+
+if __name__ == "__main__":
+    main()
