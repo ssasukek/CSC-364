ad73ef8dd89048b0be79240caa9c6065247a3710
Kevin Lu <kevinlu3234@gmail.com>
2026-01-20 11:44:50 -0800

    minor changes

--- Lab3/bmp.cpp
+++ Lab3/bmp.cpp
@@ -8,35 +8,26 @@
 
 #pragma pack(push, 1)
 struct BMPFileHeader{
-    uint16_t file_type{0x4D42};
-    uint32_t file_size{0};
-    uint16_t reserved1{0};
-    uint16_t reserved2{0};
-    uint32_t offset_data{0};
+    unsigned short bfType;      // 'BM' = 0x4D42
+    unsigned int bfSize;        // file size in bytes
+    unsigned short bfReserved1; // must be 0
+    unsigned short bfReserved2; // must be 0
+    unsigned int bfOffBits;     // offset to pixel data
 };
-
 struct BMPInfoHeader{
-    uint32_t size{0};
-    int32_t width{0};
-    int32_t height{0};
-    uint16_t planes{1};
-    uint16_t bit_count{0};
-    uint32_t compression{0};
-    uint32_t size_image{0};
-    int32_t x_pixels_per_meter{0};
-    int32_t y_pixels_per_meter{0};
-    uint32_t colors_used{0};
-    uint32_t colors_important{0};
+    unsigned int biSize;         // header size (40)
+    int biWidth;                 // image width
+    int biHeight;                // image height
+    unsigned short biPlanes;     // must be 1
+    unsigned short biBitCount;   // 24 for RGB
+    unsigned int biCompression;  // 0 = BI_RGB
+    unsigned int biSizeImage;    // image data size (can be 0 for BI_RGB)
+    int biXPelsPerMeter;         // resolution
+    int biYPelsPerMeter;         // resolution
+    unsigned int biClrUsed;      // colors used (0)
+    unsigned int biClrImportant; // important colors (0)
 };
 
-// struct BMPColorHeader{
-//     uint32_t red_mask{0x00ff0000};
-//     uint32_t green_mask{0x0000ff00};
-//     uint32_t blue_mask{0x000000ff};
-//     uint32_t alpha_mask{0xff000000};
-//     uint32_t color_space_type{0x73524742}; // 'sRGB'
-//     uint32_t unused[16]{0};
-// };
 #pragma pack(pop)
 
 struct Pixel_RGB{
@@ -51,23 +42,20 @@ struct Image{
     std::vector<Pixel_RGB> pixels;
 };
 
-static bool read_bmp(const char *filename, void *dst, size_t size){
-
-}
-
-static bool write_bmp(const char *filename, void *dst, size_t size){
-    
-}
-
 struct BMP{
     BMPFileHeader file_header;
     BMPInfoHeader info_header;
-    // BMPColorHeader color_header;
 };
 
-
-
 int main(){
+    FILE *file = fopen("test.bmp", "rb");
+    BMPFileHeader fh;
+    BMPInfoHeader ih;
+    fread(&fh, sizeof(BMPFileHeader), 1, file);
+    fread(&ih, sizeof(BMPInfoHeader), 1, file);
+
+    unsigned char *data = (unsigned char *)malloc(ih.biSizeImage);
+
     BMP bmp;
     std::cout << "Size of BMP struct: " << sizeof(bmp) << " bytes\n";
     return 0;
