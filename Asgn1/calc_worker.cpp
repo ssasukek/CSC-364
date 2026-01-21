a8644092e24dcce06d282739484998719c5daa77
Kevin Lu <kevinlu3234@gmail.com>
2026-01-21 14:59:20 -0800

    worker helper

---
+++ Asgn1/calc_worker.cpp
@@ -0,0 +1,160 @@
+#define WIN32_LEAN_AND_MEAN
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <windows.h>
+
+#include <cstdint>
+#include <cstring>
+#include <cstdlib>
+#include <thread>
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+#pragma comment(lib, "Ws2_32.lib")
+
+// Send all data in buffer
+static int send_all(SOCKET s, const char *buf, int len){
+    int total_sent = 0;
+    while (total_sent < len){
+        int n = send(s, buf + total_sent, len - total_sent, 0);
+        if (n <= 0){
+            return n;
+        }
+        total_sent += n;
+    }
+    return 1;
+}
+
+// Receive all data in buffer
+static int recv_all(SOCKET s, char *buf, int len){
+    int total_recv = 0;
+    while (total_recv < len){
+        int n = recv(s, buf + total_recv, len - total_recv, 0);
+        if (n <= 0){
+            return n;
+        }
+        total_recv += n;
+    }
+    return 1;
+}
+
+// receive data with 4-byte length
+static bool recv_data(SOCKET s, int32_t &out_msg){
+    uint32_t net_len = 0;
+    if (recv_all(s, (char *)&net_len, sizeof(net_len)) <= 0){
+        return false;
+    }
+    out_msg = (int32_t)ntohl(net_len);
+    return true;
+}
+
+// send data with 4-byte length
+static bool send_data(SOCKET s, int32_t &in_msg){
+    uint32_t net_len = 0;
+    if (send_all(s, (char *)&net_len, sizeof(net_len)) > 0){
+        return false;
+    }
+    in_msg = (int32_t)ntohl(net_len);
+    return true;
+}
+
+// contrast
+int adjust(int x, int c){
+    double f = (259.0 * (c + 255.0)) / (255.0 * (259.0 - c));
+    int y = (int)(f * (x - 128) + 128);
+    if (y < 0)
+        y = 0;
+    if (y > 255)
+        y = 255;
+    return y;
+}
+
+// Connect to TCP
+static SOCKET connect_tcp(const char *server_ip, const char *port){
+    // connect to server
+    struct addrinfo hints, *res = 0, *p = 0;
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    int info = getaddrinfo(server_ip, port, &hints, &res);
+    if (info != 0){
+        printf("getaddrinfo failed: %d\n", info);
+        return INVALID_SOCKET;
+    }
+
+    for (p = res; p; p = p->ai_next){
+        SOCKET s = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (s == INVALID_SOCKET){
+            continue;
+        }
+        if (connect(s, p->ai_addr, (int)p->ai_addrlen) == 0){
+            printf("Connected\n");
+            freeaddrinfo(res);
+            return s;
+        }
+        closesocket(s);
+    }
+    freeaddrinfo(res);
+    return INVALID_SOCKET;
+}
+
+static const char *IP = "127.0.0.1";
+static const int PORT = 5000;
+
+int main(int argc, char **argv){
+    if (argc != 4){
+        printf("Usage: calc_worker <server_ip> <server_port> <contrast_value>\n");
+        return 1;
+    }
+    const char *server_ip = argv[1];
+    const char *server_port = argv[2];
+    int contrast = atoi(argv[3]);
+
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0){
+        printf("WSAStartup failed\n");
+        return 1;
+    }
+    SOCKET s = connect_tcp(server_ip, server_port);
+    if (s == INVALID_SOCKET){
+        printf("Failed to connect to server\n");
+        WSACleanup();
+        return 1;
+    }
+
+    // math - calculatation worker
+    int32_t rows = 0, width = 0;
+    if (!recv_data(s, rows) || !recv_data(s, width)){
+        printf("Failed to receive image size\n");
+        closesocket(s);
+        WSACleanup();
+        return 1;
+    }
+    for (int i = 0; i < rows; i++){
+        vector<uint8_t> row_data(width * 3);
+        if (recv_all(s, (char *)row_data.data(), width * 3) <= 0){
+            printf("Failed to receive row data\n");
+            closesocket(s);
+            WSACleanup();
+            return 1;
+        }
+        // adjust contrast
+        for (int j = 0; j < width * 3; j++){
+            row_data[j] = (uint8_t)adjust(row_data[j], contrast);
+        }
+        // send back processed row
+        if (send_all(s, (char *)row_data.data(), width * 3) <= 0){
+            printf("Failed to send processed row data\n");
+            closesocket(s);
+            WSACleanup();
+            return 1;
+        }
+    }
+    closesocket(s);
+    WSACleanup();
+    return 0;
+}
--- Asgn1/spawn_server.cpp
+++ Asgn1/spawn_server.cpp
@@ -8,9 +8,39 @@
 #include <cstdlib>
 #include <thread>
 #include <string>
+#include <vector>
+#include <iostream>
 using namespace std;
 
 #pragma comment(lib, "Ws2_32.lib")
+
+static const char *IP = "127.0.0.1";
+static const int PORT = 5000;
+
+#pragma pack(push, 1)
+struct BMPFileHeader
+{
+    unsigned short bfType;      // 'BM' = 0x4D42
+    unsigned int bfSize;        // file size in bytes
+    unsigned short bfReserved1; // must be 0
+    unsigned short bfReserved2; // must be 0
+    unsigned int bfOffBits;     // offset to pixel data
+};
+struct BMPInfoHeader
+{
+    unsigned int biSize;         // header size (40)
+    int biWidth;                 // image width
+    int biHeight;                // image height
+    unsigned short biPlanes;     // must be 1
+    unsigned short biBitCount;   // 24 for RGB
+    unsigned int biCompression;  // 0 = BI_RGB
+    unsigned int biSizeImage;    // image data size (can be 0 for BI_RGB)
+    int biXPelsPerMeter;         // resolution
+    int biYPelsPerMeter;         // resolution
+    unsigned int biClrUsed;      // colors used (0)
+    unsigned int biClrImportant; // important colors (0)
+};
+#pragma pack(pop)
 
 // Send all data in buffer
 static int send_all(SOCKET s, const char *buf, int len){
@@ -38,128 +68,55 @@ static int recv_all(SOCKET s, char *buf, int len){
     return 1;
 }
 
-// sending 4 byte legnth framing
-static bool send_framing(SOCKET s, const char *buf, int len){
-    uint32_t net_len = htonl(len);
-    if (send_all(s, (char *)&net_len, sizeof(net_len)) <= 0){   // 4 bytes
-        return false;
-    }
-    if (len == 0){
-        return true;
-    }
-    return send_all(s, buf, len) > 0;
-}
-
-// receive framed message
-static bool recv_framing(SOCKET s, std::string &out_msg){
+// send data with 4-byte length
+static bool send_data(SOCKET s, int32_t &in_msg){
     uint32_t net_len = 0;
-    if (recv_all(s, (char *)&net_len, sizeof(net_len)) <= 0){
+    if (send_all(s, (char *)&net_len, sizeof(net_len)) > 0){
         return false;
     }
-    uint32_t len = ntohl(net_len);
-    out_msg.resize(len);
-    if (len == 0){
-        return true;
-    }
-    return recv_all(s, out_msg.data(), len) > 0;
+    in_msg = (int32_t)ntohl(net_len);
+    return true;
 }
 
-struct relay_args{
-    SOCKET source_s;
-    SOCKET dest_s;
-    volatile LONG *stop_event;
-    const char *direction;
-};
-
-// relay data concurrently in both directions
-static DWORD WINAPI tcp_relay(LPVOID p){
-    relay_args *args = (relay_args *)p;
-
-    while (*(args->stop_event) == 0){
-        std::string msg;
-
-        if (!recv_framing(args->source_s, msg)){
-            printf("Connection closed in direction %s\n", args->direction);
-            *(args->stop_event) = 1;
-            break;
-        }
-
-        if (!send_framing(args->dest_s, msg.data(), (int)msg.size())){
-            printf("Failed to send in direction %s\n", args->direction);
-            *(args->stop_event) = 1;
-            break;
-        }
+// read BMP image
+static bool readImage(const char *filename, char **pixels, int32_t *width, int32_t *height, int32_t *size)
+{
+    FILE *imageFile = fopen(filename, "rb");
+    if (!imageFile){
+        return false;
     }
-
-    // unblock other direction
-    shutdown(args->dest_s, SD_BOTH);
-    shutdown(args->source_s, SD_BOTH);
-    return 0;
-
+    int32_t dataSize = 0;
+    fseek(imageFile, 0, SEEK_END);
 }
+
+// write BMP image
+static bool writeImage(const char *filename, const char *data, int size)
+{
+    FILE *file = fopen(filename, "wb");
+    if (!file){
+        return false;
+    }
+    fwrite(data, 1, size, file);
+    fclose(file);
+    return true;
+}
+
+
 
 int main(int argc, char **argv)
 {
-    int port = atoi(argv[1]);
-    WSADATA w;
-    WSAStartup(MAKEWORD(2, 2), &w);
-
-    SOCKET ls = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-    int yes = 1;
-    setsockopt(ls, SOL_SOCKET, SO_REUSEADDR, (const char *)&yes, sizeof(yes));
-
-    struct sockaddr_in addr = {0};
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    addr.sin_port = htons(port);
-
-    bind(ls, (struct sockaddr *)&addr, sizeof(addr));
-    listen(ls, 5);
-    printf("Listening on port %d\n", port);
-
-    for (;;)
-    {
-        printf("Waiting for connection A...\n");
-        SOCKET a = accept(ls, NULL, NULL);
-        if (a == INVALID_SOCKET)
-            continue;
-        printf("Connected to A\n");
-
-        printf("Waiting for connection B...\n");
-        SOCKET b = accept(ls, NULL, NULL);
-        if (b == INVALID_SOCKET){
-            closesocket(a);
-            continue;
-        }
-        printf("Connected to B\n");
-
-        // send READY to both clients
-        const char *ready_msg = "READY\n";
-        if(!send_all(a, ready_msg, (int)strlen(ready_msg)) || !send_all(b, ready_msg, (int)strlen(ready_msg))){
-            printf("Failed to send READY\n");
-            closesocket(a);
-            closesocket(b);
-            continue;
-        }
-
-        volatile LONG stop_event = 0;
-        relay_args args1 = { a, b, &stop_event, "A->B" };
-        relay_args args2 = { b, a, &stop_event, "B->A" };
-
-        HANDLE t1 = CreateThread(NULL, 0, tcp_relay, &args1, 0, NULL);
-        HANDLE t2 = CreateThread(NULL, 0, tcp_relay, &args2, 0, NULL);
-
-        WaitForSingleObject(t1, INFINITE);
-        WaitForSingleObject(t2, INFINITE);
-        CloseHandle(t1);
-        CloseHandle(t2);
-
-        closesocket(a);
-        closesocket(b);
+    if (argc != 3){
+        printf("Usage: spawn_server <port> <output_file>");
+        return 1;
     }
 
-    closesocket(ls);
-    WSACleanup();
-    return 0;
+    BMPFileHeader *fileHeader = (BMPFileHeader *)bmp.data();
+    BMPInfoHeader *infoHeader = (BMPInfoHeader *)(bmp.data() + sizeof(BMPFileHeader));
+
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0){
+        printf("WSAStartup failed\n");
+        return 1;
+    }
+
 }
